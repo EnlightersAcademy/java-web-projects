2e5a23dba797b7111b13324849c1b4cb6a43e0c0
Rajagopal <rmanohar@cisco.com>
2020-02-27 19:05:38 +0530

    checkin hibernate files

---
+++ BookMyGame/src/com/bookmygame/services/impl/CustomerServicesImpl.java
@@ -0,0 +1,34 @@
+package com.bookmygame.services.impl;
+
+import java.util.List;
+
+import com.bookmygame.pojo.Customer;
+import com.bookmygame.pojo.GameBooking;
+import com.bookmygame.services.CustomerServices;
+
+public class CustomerServicesImpl implements CustomerServices {
+
+	public void registerCustomer(Customer customer) {
+
+	}
+
+	public Customer loginCustomer(String userName, String password) {
+		return null;
+	}
+
+	public void updateCustomerDetails(Customer customer) throws Exception {
+
+	}
+
+	public List<GameBooking> getAllBookings(int customerId) {
+		return null;
+	}
+
+	public void bookMyGame(GameBooking booking) throws Exception {
+
+	}
+
+	public void cancelBooking(GameBooking booking) throws Exception {
+
+	}
+}
